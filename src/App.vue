adf117b6864db40e313ac2fd5c22b5de1fd438fb
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-14 16:59:39 +0200

    Bonus: feat: add a Jumbotron in the Hero component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
 
     <Title />
+
+    <Hero />
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 
 export default {
   name: 'App',
   components: {
     Title,
-  }
+    Hero
+}
 }
 </script>
 
